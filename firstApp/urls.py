286a1561d575ab864d9e0848e5782c137775b972
Prem Kumar <premkumarmvks@gmail.com>
2023-11-03 13:18:56 +0530

    loops

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('myapp', views.index),
-    path('myapp2', views.index2),
-    path('<int:app>', views.handle_all_int),
-    path('<str:app>', views.handle_all, name="apps-num"),
-    path('', views.root_page)
+    path('', views.root_page),
+    path('loop', views.loop),
 ]
 
--- firstApp/views.py
+++ firstApp/views.py
@@ -4,51 +4,18 @@ from django.urls import reverse
 from django.shortcuts import render
 # Create your views here.
 
-def index(Request):
-    return HttpResponse("Hi")
-
-def index2(Request):
-    return HttpResponse("Hi2")
-
-def _handle_all(request, app):
-    text = None
-
-    if app=='app1':
-        text = "Hi1"
-    elif app=='app2':
-        text="Hi2"
-    else:
-        return HttpResponseNotFound()
-    
-    return HttpResponse(text)
-
-apps = {
-    'app1': 'Hi1',
-    'app2': 'Hi2'
-}
-
-def handle_all_int(request, app):
-    apps_key = list(apps.keys())
-    try:
-        apps_name = apps_key[app]
-
-        #apps-num is the name of the url specifiyed in django apps urls.py file
-        redirect_path = reverse('apps-num', args=[apps_name])
-
-        return HttpResponseRedirect(redirect_path)
-    except:
-        return HttpResponseNotFound()
-
-
-def handle_all(request, app):
-    if app in apps.keys():
-        return HttpResponse(apps[app])
-    else:
-        return HttpResponseNotFound()
-    
-
 def root_page(request):
     return render(request, 'firstApp/firstApp.html', {'test': 'from python'})
+
+
+def loop(request):
+    lst = [i for i in range(1,10)]
+    
+    return render(request, 'firstApp/loop.html', {
+            'list_django': lst
+        })
+
+
 
 
         
