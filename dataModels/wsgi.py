f5d672fc494dfb6f815f10498500999ff499fa21
Prem Kumar <premkumarmvks@gmail.com>
2023-11-04 18:01:55 +0530

    Create migrations

--- dataModels/wsgi.py
+++ dataModels/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoCourse project.
+WSGI config for dataModels project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCourse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dataModels.settings')
 
 application = get_wsgi_application()
