42d7e75ebe201f35da637cf75c11a57ab503076c
Prem Kumar <premkumarmvks@gmail.com>
2023-11-05 08:47:36 +0530

    Model and relations

--- books/models.py
+++ books/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    author = models.CharField(max_length=100, default="Prem")
+
+    def __str__(self) -> str:
+        return f"{self.title}, has rating of {self.rating}"
